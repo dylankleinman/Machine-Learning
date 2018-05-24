b9187de417ddade66575f67b3cb490c2a81e009f
dylankleinman <dylankleinman@yahoo.com>
2018-05-24 11:55:44 -0700

    these dont work, reverting back

--- preprocess.py
+++ preprocess.py
@@ -8,11 +8,12 @@ sys.setdefaultencoding('utf-8')
 
 stop_words = set(stopwords.words("english"))
 list = []
-file = open('alex_file.txt','w')
+file = open('output.txt','w')
 with open('train_file_cmps142_hw3', 'r') as fileinput:
    for line in fileinput:
        lines = line.lower()
-       token = word_tokenize(line)
+       file.write(lines)
+       token = word_tokenize(lines)
        list.append(token)
 file.close()
 
@@ -22,19 +23,20 @@ def unique(list):
 		if x not in unique_list:
 			unique_list.append(x)
 	print('the unique list length: ')
+	print unique_list[0]
 	print len(unique_list)
 
-unique(list)
+unique(list[0])
 print('the regular length is')
 print len(list)
 
-example_sent = ['hello','hello','there','in', 'is']
-filtered_word_list = example_sent
+# example_sent = ['hello','hello','there','in', 'is']
+# filtered_word_list = example_sent
 
-for word in example_sent: # iterate over word_list
-  if word in stopwords.words('english'): 
-    filtered_word_list.remove(word)
+# for word in example_sent: # iterate over word_list
+#   if word in stopwords.words('english'): 
+#     filtered_word_list.remove(word)
 
-print('the words in example_sent with stopwords taken out:')
-print filtered_word_list
+# print('the words in example_sent with stopwords taken out:')
+# print filtered_word_list
 
