0c14cff418e1c5a191ce959e90fd984faa7b7883
dylankleinman <dylankleinman@yahoo.com>
2018-05-24 15:16:34 -0700

    found total number of words, as well as number of distinct words

--- preprocess.py
+++ preprocess.py
@@ -8,16 +8,40 @@ sys.setdefaultencoding('utf-8')
 
 stop_words = set(stopwords.words("english"))
 list = []
+distinct_list = []
 rem_stopwords_list = []
 stopwords_set =  set(stopwords.words('english'))
 file = open('output.txt','w')
-with open('train_file_cmps142_hw3', 'r') as fileinput:
-   for line in fileinput:
-       lines = line.lower()
-       file.write(lines)
-       token = word_tokenize(lines)
-       list.append(token)
+distinct_file = open('output_distinct.txt','w')
+numwords = 0
+numlines = 0
 
+with open('train_file_cmps142_hw3', 'r') as fileinput:
+	for line in fileinput:
+		lines = line.lower()
+		file.write(lines)
+		token = word_tokenize(lines)
+		list.append(token)
+
+
+file.close()
+
+##count number of words in file
+with open('output.txt', 'r') as file:
+	for line in file:
+		wordslist = line.split()
+		for word in wordslist:
+			if word not in distinct_list:
+				distinct_list.append(word)
+		numwords += len(wordslist)
+
+print('the number of words in the textfile is:')
+print numwords
+print('the number of distinct words in the textfile is: ')
+print len(distinct_list)
+
+
+##doesnt work on list elements yet, only lists within the list
 unique_list = []
 def unique(list):
 	for x in list:
@@ -25,27 +49,15 @@ def unique(list):
 			unique_list.append(x)
 	print('the unique list length: ')
 	print len(unique_list)
+#############################################
+# unique(list)
+# print('the regular length is')
+# print len(list)
 
-
-unique(list)
-print('the regular length is')
-print len(list)
-
+####removes stop words from the list and puts in new list
 rem_stopwords_list = [[word for word in sub if word not in stopwords_set] for sub in list]
-print rem_stopwords_list
+#print rem_stopwords_list
 
-
-# example_sent = ['hello','hello','there','in', 'is', 'dylan'],['hello','hello','there','in', 'is','alex'],['hello','hello','there','in', 'is']
-# unique(example_sent)
-# print ('this is the unfiltered example_sent: ')
-# print example_sent
-
-# example_sent = [[word for word in sub if word not in stopwords_set] for sub in example_sent]
-# print example_sent
-
-
-
-file.close()
 
 
 
